5017b5ed325d980b40153a14059eff274aec4c15
Haley Jones <nyanidesuka@icloud.com>
2019-05-03 16:01:45 -0600

    Initial Commit

---
+++ GitLesson2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitLesson2
+//
+//  Created by Haley Jones on 5/3/19.
+//  Copyright © 2019 HaleyJones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
